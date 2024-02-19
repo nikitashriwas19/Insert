917e4f2b95ef6fe58ea6adb418d4e66f592c6657
Nikita Shriwas <nikitashriwas98@gmail.com>
2024-02-19 10:50:03 +0530

    print 1 to 10 number

---
+++ src/com/addition/PrintNo.java
@@ -0,0 +1,12 @@
+package com.addition;
+
+public class PrintNo {
+
+	public static void main(String[] args) {
+		for(int i=1; i<=10; i++) {
+			System.out.println("Print Number 1 to 10");
+		}
+
+	}
+
+}
